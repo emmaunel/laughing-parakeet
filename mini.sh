1eeaa16b0fcecb2922317590ea707c7ecec91132
emmaunel <aea8506@rit.edu>
2020-04-15 04:06:56 -0400

    done

--- mini.sh
+++ mini.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+PS1outfile="APM1_metrics.csv"
+echo -e "time %CPU %MEM" >> $PS1outfile
+PS2outfile="APM2_metrics.csv"
+echo -e "time %CPU %MEM" >> $PS2outfile
+PS3outfile="APM3_metrics.csv"
+echo -e "time %CPU %MEM" >> $PS3outfile
+PS4outfile="APM4_metrics.csv"
+echo -e "time %CPU %MEM" >> $PS4outfile
+PS5outfile="APM5_metrics.csv"
+echo -e "time %CPU %MEM" >> $PS5outfile
+PS6outfile="APM6_metrics.csv"
+echo -e "time %CPU %MEM" >> $PS6outfile
+
+systemlog="system_metrics.csv"
+echo -e "Time RX-Data TX-Data Disk-Write Disk-Use" >> $systemlog
+
+
 #Arguments: IP address of NIC
 function startProcesses {
 	./APMs/APM1 $1 &
@@ -12,22 +29,35 @@ function startProcesses {
 	ifstat -d 1
 }
 
+#Arguments: time
 function processlevel {
 	# ps -p <pid> -o %cpu,%mem,cmd
 	#sleep 5
-	local P1=$(pidof APM1)
-	local P2=$(pidof APM2)
-	local P3=$(pidof APM3)
-	local P4=$(pidof APM4)
-	local P5=$(pidof APM5)
-	local P6=$(pidof APM6)
+	P1=$(pidof APM1)
+	P2=$(pidof APM2)
+	P3=$(pidof APM3)
+	P4=$(pidof APM4)
+	P5=$(pidof APM5)
+	P6=$(pidof APM6)
 
-	ps -p $P1 -o %cpu,%mem,cmd
-	ps -p $P2 -o %cpu,%mem,cmd
-	ps -p $P3 -o %cpu,%mem,cmd
-	ps -p $P4 -o %cpu,%mem,cmd
-	ps -p $P5 -o %cpu,%mem,cmd
-	ps -p $P6 -o %cpu,%mem,cmd
+	
+	PS1=$(ps -p $P1 -o %cpu,%mem,cmd | tail -n 1)
+	echo "${1} , $PS1" >> $PS1outfile
+
+	PS2=$(ps -p $P2 -o %cpu,%mem,cmd | tail -n 1)
+	echo "${1} , $PS2" >> $PS2outfile
+
+	PS3=$(ps -p $P3 -o %cpu,%mem,cmd | tail -n 1)
+	echo "${1} , $PS3" >> $PS3outfile
+
+	PS4=$(ps -p $P4 -o %cpu,%mem,cmd | tail -n 1)
+	echo "${1} , $PS4" >> $PS4outfile
+
+	PS5=$(ps -p $P5 -o %cpu,%mem,cmd | tail -n 1)
+	echo "${1} , $PS5" >> $PS5outfile
+
+	PS6=$(ps -p $P6 -o %cpu,%mem,cmd | tail -n 1)
+	echo "${1} , $PS6" >> $PS6outfile
 }
 
 function cleanup {
@@ -43,11 +73,11 @@ function cleanup {
 
 function system() {
 	local DISKW=$(iostat -ky 5 1 | grep sda | awk '{print $4}')
-	local IFOUT=$(ifstat ens192 | grep ens192)
+	local IFOUT=$(ifstat lo | grep lo)
 	local RX=$(printf "$IFOUT" | awk '{print $6}')
 	local TX=$(printf "$IFOUT" | awk '{print $8}')
 	local DISKUSE=$(df / -B MB | grep /dev/mapper/centos-root | awk '{print $3}')
-	logSystem 5 "$RX" "$TX" "$DISKW" "$DISKUSE"
+	logSystem ${1} "$RX" "$TX" "$DISKW" "$DISKUSE"
 }
 
 #Arguments: process name, seconds, %cpu, %memory
@@ -58,13 +88,21 @@ function logProcess {
 
 #Arguments: seconds, RX rate, TX rate, disk writes, disk capacity
 function logSystem {
-	outfile="system_metrics.csv"
-	echo "${1}, ${2}, ${3}, ${4}, ${5}" >> $outfile
+	echo "${1}, ${2}, ${3}, ${4}, ${5}" >> $systemlog
 }
 
+end=900
+SECONDS=0
+
+cleanup
 startProcesses 127.0.0.1
-system
-processlevel
-sleep 5
+while [ $SECONDS -lt $end ]; do
+	echo $SECONDS
+	sleep 5
+	# Monitor process
+	processlevel $SECONDS
+	system $SECONDS
+done
+
 cleanup
 
